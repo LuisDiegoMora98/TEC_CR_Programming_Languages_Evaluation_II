88f66ce3ef178cfa77cb5866d6ad598b4f949bb0
ChesGuev99 <anaguevarabonilla@gmail.com>
2021-10-13 20:31:14 -0600

    item 3

--- main.go
+++ main.go
@@ -187,3 +187,18 @@ func print(w io.Writer, node *BinaryNode, ns int, ch rune) {
 	print(w, node.left, ns+2, 'L')
 	print(w, node.right, ns+2, 'R')
 }
+
+//Item 3, receive an int Array and a nalue to insert, then it compares all elements in the array to the new value
+//returns the number of comparisons until the number insertion or to determine the number is already in the array
+func item3(arrayTo []int, newValue int) int {
+	comparisons := 0
+	for k := range arrayTo {
+		if arrayTo[k] == newValue {
+			return comparisons
+		}
+		comparisons++
+	}
+	arrayTo = append(arrayTo, newValue)
+	fmt.Println(arrayTo)
+	return comparisons
+}
